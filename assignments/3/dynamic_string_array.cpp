270107d10f3f2e4a8661e0453cff928178b12cf3
Derek Lopes <derek@Dereks-Mac-Pro.local>
2015-04-08 20:58:49 -0700

    fixed the majority of issues

--- assignments/3/dynamic_string_array.cpp
+++ assignments/3/dynamic_string_array.cpp
@@ -9,9 +9,9 @@
 /* Default constructor to initialize the array to size 10 and elements to NULL
  * Sets amount of elements to 0.
  */
-DynamicStringArray() {
+DynamicStringArray::DynamicStringArray() {
   max_capacity = 10;
-  string_array = new string[max_capacity];    
+  string_array = new string*[max_capacity];    
   size = 0;
   for (unsigned int i = 0; i < max_capacity; i++) {
     string_array[i] = NULL;
@@ -21,9 +21,9 @@ DynamicStringArray() {
  * Defaults to size 10 if parameter is invalid.
  * @param unsigned int - size the array should be initialized
  */
-DynamicStringArray(unsigned int size) {
+DynamicStringArray::DynamicStringArray(unsigned int size) {
   max_capacity = size;
-  string_array = new string[max_capacity];    
+  string_array = new string*[max_capacity];    
   size = 0;
   for (unsigned int i = 0; i < max_capacity; i++) {
     string_array[i] = NULL;
@@ -32,7 +32,7 @@ DynamicStringArray(unsigned int size) {
 
 /* Destructor to free all memory allocated to the arrays and strings.
  */
-~DynamicStringArray() {
+DynamicStringArray::~DynamicStringArray() {
   size = 0;
   for (unsigned int i = 0; i < max_capacity; i++) {
     string_array[i] = NULL;
@@ -45,41 +45,41 @@ DynamicStringArray(unsigned int size) {
  * @param unsigned int - the location to find the string
  * @return string* - pointer to the string at provided location
  */
-string* At(unsigned int location) {
-  if (location > size || location < 0)
+string* DynamicStringArray::At(unsigned int location) {
+  if (location > size)
     throw string("Invallid Location");
-  return &string_array[location];
+  return string_array[location];
 }
 
 /* Returns a pointer to the first string in the array.
  * Throws "Array Empty" if the array is empty.
  * @return string* - pointer to the first string in the array.
  */
-string* GetFirst() {
+string* DynamicStringArray::GetFirst() {
   if (size != 0)
     throw string("Invallid Location");
-  return &string_array[0];
+  return string_array[0];
 }
 
 /* Returns a pointer to the last string in the array.
  * Throws "Array Empty" if the array is empty.
  * @return string* - pointer to the last string in the array.
  */
-string* GetLast() {
+string* DynamicStringArray::GetLast() {
   if (size != 0)
     throw string("Invallid Location");
-  return &string_array[size];
+  return string_array[size];
 }
 
 /* Adds the provided string to the front of the array.
  * If the array is full, the capacity is increased first.
  * @param string* - string to add to the array
  */
-void AddFront(string* to_add) {
+void DynamicStringArray::AddFront(string* to_add) {
   if (size == max_capacity) {
     IncreaseCapacity();
   }
-  string* string_array_2 = new string[max_capacity];
+  string** string_array_2 = new string*[max_capacity];
   string_array_2[0] = to_add;
   for (unsigned int i = 1; i < max_capacity; i++) {
     string_array_2[i] = string_array[i - 1];
@@ -94,7 +94,7 @@ void AddFront(string* to_add) {
  * If the array is full, the capacity is increased frist.
  * @param string* - string to add to the array
  */
-void AddBack(string* to_add) {
+void DynamicStringArray::AddBack(string* to_add) {
   if (size == max_capacity) {
     IncreaseCapacity();
   }
@@ -104,10 +104,10 @@ void AddBack(string* to_add) {
 /* Deletes the first element in the array.
  * If the array is empty, the string "Array Empty" is thrown.
  */
-void DeleteFront() {
+void DynamicStringArray::DeleteFront() {
   if (size == 0)
     throw string("Array Empty");
-  string* string_array_2 = new string[max_capacity];
+  string** string_array_2 = new string*[max_capacity];
   for (unsigned int i = 1; i < size; i++) {
     string_array_2[i - 1] = string_array[i];
   }
@@ -119,41 +119,41 @@ void DeleteFront() {
 /* Deletes the last element in the array.
  * If the array is empty, the string "Array Empty" is thrown.
  */
-void DeleteBack() {
-  the_array[size - 1] = NULL;
+void DynamicStringArray::DeleteBack() {
+  string_array[size - 1] = NULL;
   size--;
 }
 
 /* Const function that gets the current size of the array.
  * @return unsigned int - the current size of the array
  */
-unsigned int GetSize() const {
+unsigned int DynamicStringArray::GetSize() const {
   return size;
 }
   
 /* Const function that gets the current maximum capacity of the array.
  * @return unsigned int - the maximum capacity of the array
  */
-unsigned int GetCapacity() const {
+unsigned int DynamicStringArray::GetCapacity() const {
   return max_capacity;
 }
 
 /* Const function. Checks to see if the array is empty.
  * @return bool - returns true if array is empty (size 0)
  */
-bool Empty() const {
+bool DynamicStringArray::Empty() const {
   if (size == 0) {
     return true;
   } else {
-    reutrn false;
+    return false;
   }
 }
   
 /* Deletes all of the strings in the array and sets size to 0.
  */
-void Clear() {
+void DynamicStringArray::Clear() {
   for (unsigned int i = 0; i < size; i++) {
-    the_array[i] = NULL;
+    string_array[i] = NULL;
   }
   size = 0;
 }
@@ -161,26 +161,14 @@ void Clear() {
 /* Sorts the array alphabetically A-Z, case insensitive.
  * @uses ToUpper()
  */
-void Sort() {
-  unsigned int gap = size / 2;
-  while (gap > 0) {
-    for (unsigned int i = gap; i < size; i++) {
-      string temp = string_array[i];
-      unsigned int j = i;
-      while (j >= gap && ToUpper(string_array[j - gap]).at(1) > temp) {
-        string_array[j] = string_array[j - gap];
-        j = j - gap;
-      }
-      string_array[j] = temp;
-    }
-    gap = gap / 2;
-  }
+void DynamicStringArray::Sort() {
+
 }
 
 /* Returns a string containing all the strings in the array, comma separated.
  * @return string - a string of all the strings in the array
  */
-void ToString() {
+string DynamicStringArray::ToString() {
   stringstream ss;
   for (unsigned int i = 0; i < (size - 1); i++) {
     ss << string_array[i] << ", ";
@@ -191,25 +179,22 @@ void ToString() {
 
 /* Overloaded Friend Operator for << to output a comma separated list of the strings
  */
-const ostream& operator <<(ostream& out, const DynamicStringArray& array) {
-  for (unsigned int i = 0; i < (size - 1); i++) {
-    out << the_array[i] << ", "; 
-  }
-  out << the_array[size - 1];
+ostream& operator <<(ostream& out, const DynamicStringArray& the_array) {
+  out << the_array.ToString();  
   return out;
 }
 
 /* Increases the capacity of the array by 10.
  */
-void IncreaseCapacity() {
-  string* string_array_2 = new string[max_capacity + 10];
+void DynamicStringArray::IncreaseCapacity() {
+  string** string_array_2 = new string*[max_capacity + 10];
   for (unsigned int i = 0; i < size; i++) {
     string_array_2[i] = string_array[i];
   }
   for (unsigned int i = size; i < max_capacity; i++) {
     string_array_2[i] = NULL;
   }
-  delete string_array[];
+  delete[] string_array;
   string_array = string_array_2;
   string_array_2 = NULL;
 }
