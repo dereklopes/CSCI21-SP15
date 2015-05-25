34026fd09bdd4554421845da6cf8cdf826ad0a20
root <root@debian.lopes.com>
2015-05-25 16:00:42 -0700

    Updated assignment 5 files.

--- assignments/5/bs_treet.h
+++ assignments/5/bs_treet.h
@@ -98,13 +98,13 @@ class BSTreeT {
    * @param BSTNode* - the root to start at
    * @return string - the values of the tree in order
    */
-  string ToStringForwards(BSTNodeT<T>* root);
+  string ToStringForwards(BSTNodeT<T>* root, T last_value);
 
   /* Creates a string of the tree values in reverse order
    * @param BSTNode* - the root to start at
    * @return string - the values of the tree in reverse order
    */
-  string ToStringBackwards(BSTNodeT<T>* root);
+  string ToStringBackwards(BSTNodeT<T>* root, T first_value);
 
   /* Deletes the given value in the given tree.
    * Decrements count if the value exists.
@@ -120,5 +120,256 @@ class BSTreeT {
    */
   BSTNodeT<T>* Get(T value, BSTNodeT<T>* root);
 };
+
+// PUBLIC MEMEBER FUNCTIONS
+
+/* Default constructor. Sets root to NULL and size to 0.
+ */
+template <typename T>
+BSTreeT<T>::BSTreeT()
+  : root_(NULL), size_(0) { }
+
+/* Destructor. Calls the clear function.
+ */
+template <typename T>
+BSTreeT<T>::~BSTreeT() {
+  Clear();
+}
+
+/* Gets the size of the tree.
+ * @return unsigned int - the size of the tree
+ */
+template <typename T>
+unsigned int BSTreeT<T>::GetSize() const {
+  return size_;
+}
+
+/* Clears the entire tree. Calls the private clear funciton.
+ */
+template <typename T>
+void BSTreeT<T>::Clear() {
+  Clear(root_);
+}
+
+/* Inserts a value into the tree. Calls the private insert function.
+ * @param T - the value to add to the tree
+ * @return bool - true if the value was inserted
+ */
+template <typename T>
+unsigned int BSTreeT<T>::Insert(T value) {
+  return Insert(value, root_);
+}
+
+/* Searches the tree for the given value.
+ * @param T - the value to search for
+ * @return bool - true if the value was found
+ */
+template <typename T>
+bool BSTreeT<T>::Exists(T value) {
+  return Exists(value, root_);
+}
+
+/* Calls the private remove function
+ * @param T - the value to remove
+ * @return int - count, or -1 if it does not exist
+ */
+template <typename T>
+int BSTreeT<T>::Remove(T value) {
+  if (root_ == NULL)
+    return (-1);
+  return Remove(value, root_);
+}
+
+/* Calls the private get function 
+ * @param T - the value to search for
+ * @return BSTNodeT* - a pointer to the node searched for, NULL if not found 
+ */
+template <typename T>
+BSTNodeT<T>* BSTreeT<T>::Get(T value) {
+  return Get(value, root_);
+}
+
+/* Returns a string of the values in order
+ * The values are separated by a comma
+ * @return string - the values of the tree in order
+ */
+template <typename T>
+string BSTreeT<T>::ToStringForwards() {
+  // Finds the last value in the tree so that no extra comma is added
+  T last_value;
+  if (root_ == NULL) {
+    return string(""); 
+  } else {
+    BSTNodeT<T> *root = root_;
+    while (root->GetRightChild() != NULL) {
+      root = root->GetRightChild();
+    }
+    last_value = root->GetContents();
+  }
+  return ToStringForwards(root_, last_value);
+}
+
+/* Returns a string of the values in reverse order
+ * The values are separated by a comma
+ * @return string - the values of the tree in reverse order
+ */
+template <typename T>
+string BSTreeT<T>::ToStringBackwards() {
+  // Finds the first value in the tree so that no extra comma is added
+  T first_value;
+  if (root_ == NULL) {
+    return string(""); 
+  } else {
+    BSTNodeT<T> *root = root_;
+    while (root->GetLeftChild() != NULL) {
+      root = root->GetLeftChild();
+    }
+    first_value = root->GetContents();
+  }
+  return ToStringBackwards(root_, first_value);
+}
+
+// PRIVATE MEMBER FUNCTIONS
+
+/* Inserts the value into the tree starting at the root provided
+ * Increments count if the value is already present
+ * @param T - value to insert into the tree
+ * @param BSTNode*& - the root to start at
+ * @return unsigned int - the number of times that value has been inserted 
+ */
+template <typename T>
+unsigned int BSTreeT<T>::Insert(T value, BSTNodeT<T>*& root) {
+  if (root == NULL) {
+    root = new BSTNodeT<T>(value);
+    size_++;
+    return root.GetCount();
+  }
+  if (root->GetContents() == value) {
+    root.IncrementCount();
+    return root.GetCount();
+  }
+  if (root->GetContents() > value) {
+    return Insert(value, root->GetLeftChild());
+  }
+  if (root->GetContents() < value) {
+    return Insert(value, root->GetRightChild());
+  }
+  return (-1);  // for error checking, should never reach this
+}
+
+/* Clears the tree at the root provided.
+ * @param BSTNODE*& - the root to clear of the tree
+ */
+template <typename T>
+void BSTreeT<T>::Clear(BSTNodeT<T>*& root) {
+  if (root == NULL)
+    return;
+  Clear(root->GetLeftChild());
+  Clear(root->GetRightChild());
+  delete root;
+  size_--;
+}
+
+/* Creates a string of the tree values in order
+ * @param BSTNode* - the root to start at
+ * @return string - the values of the tree in order
+ */
+template <typename T>
+string BSTreeT<T>::ToStringForwards(BSTNodeT<T>* root, T last_value) {
+  if (root == NULL)
+    return string("");
+  stringstream ss;
+  if (root->GetContents() == last_value) {
+    ss << root->GetContents();
+  } else {
+    ss << InOrder(root->GetLeftChild());
+    ss << root->GetContents() << " ";
+    ss << InOrder(root->GetRightChild());
+  } 
+  return ss.str();
+}
+
+/* Creates a string of the tree values in reverse order
+ * @param BSTNode* - the root to start at
+ * @return string - the values of the tree in reverse order
+ */
+template <typename T>
+string BSTreeT<T>::ToStringBackwards(BSTNodeT<T>* root, T first_value) {
+  if (root == NULL)
+    return string("");
+  stringstream ss;
+  if (root->GetContents() == first_value) {
+    ss << root->GetContents();
+  } else {
+    ss << InOrder(root->GetRightChild());
+    ss << root->GetContents() << " ";
+    ss << InOrder(root->GetLeftChild());
+  } 
+  return ss.str();
+}
+
+/* Deletes the given value in the given tree.
+ * Decrements count if the value exists.
+ * @param T - the value to remove
+ * @param BSTNode*& - the root of the tree to search
+ * @return int - count, or -1 if it does not exist
+ */
+template <typename T>
+int BSTreeT<T>::Remove(T value, BSTNodeT<T>*& root) {
+  if (value == root->GetContents()) {
+    if (root->GetCount() > 0) {
+      root->DecrementCount();
+      return root->GetCount();
+    }
+    if (root->GetLeftChild() == NULL && root->GetRightChild() == NULL) {
+      delete root;
+      root = NULL;
+      size_--;
+      return true;
+    } else if (root->GetLeftChild() == NULL) {
+      BSTNodeT<T>* temp = root;
+      root = root->GetRightChild();
+      delete temp;
+      size_--;
+      return true;
+    } else if (root->GetRightChild() == NULL) {
+      BSTNodeT<T>* temp = root;
+      root = root->GetLeftChild();
+      delete temp;
+      size_--;
+      return true;
+    } else {
+      root->SetContents(FindMin(root->GetRightChild()));
+      return Remove(root->GetContents(), root->GetRightChild());
+    }
+  } else if (value > root->GetContents() && root->GetRightChild() != NULL) {
+    return Remove(value, root->GetRightChild());
+  } else if (value < root->GetContents() && root->GetLeftChild() != NULL) {
+    return Remove(value, root->GetLeftChild());
+  } else {
+    return (-1);
+  }
+}
+
+
+/* Gets a pointer to the node containing the provided value.
+ * @param T - the value to search for
+ * @return BSTNodeT* - a pointer to the node searched for, NULL if not found 
+ */
+template <typename T>
+BSTNodeT<T>* BSTreeT<T>::Get(T value, BSTNodeT<T>* root) {
+  if (value == root->GetContents())
+    return root;
+  if (root->GetLeftChild() == NULL && root->GetRightChild() == NULL) {
+    return NULL;
+  } else if (value < root->GetContents() && root->GetLeftChild() != NULL) {
+    return Get(value, root->GetLeftChild());
+  } else if (value > root->GetContents() && root->GetRightChild() != NULL) {
+    return Get(value, root->GetRightChild());
+  } else {
+    return NULL;
+  }
+}
+
 
 #endif  // BSTREET_LOPES
--- assignments/5/bst_nodet.h
+++ assignments/5/bst_nodet.h
@@ -74,4 +74,95 @@ class BSTNodeT {
   BSTNodeT *left_child_, *right_child_;
 };
 
+/* Default constructor. Sets left and right pointers to NULL, count to 0,
+ * and contents to default.
+ */
+template <typename T>
+BSTNodeT<T>::BSTNodeT()
+  : contents_(T()), count_(0), left_child_(NULL), right_child_(NULL) { } 
+
+/* Overloaded constructor. Sets left and right pointers to NULL.
+ * Sets the contents to the provided value. Sets count to 0.
+ * @param T - value to set contents of node to
+ */
+template <typename T>
+BSTNodeT<T>::BSTNodeT(T value)
+  : contents_(value), count_(0), left_child_(NULL), right_child_(NULL) { } 
+
+/* Destructor. Sets left and right pointers to NULL.
+ */
+template <typename T>
+BSTNodeT<T>::~BSTNodeT() {
+  left_child_ = right_child_ = NULL;
+}
+
+/* Sets the contents of the node.
+ * @param T - value to set contents of node to
+ */
+template <typename T>
+void BSTNodeT<T>::SetContents(T value) {
+  contents_ = value;
+}
+
+/* Gets the contents of the node. Const function.
+ * @return T - the contents of the node
+ */
+template <typename T>
+T BSTNodeT<T>::GetContents() const {
+  return contents_;
+}
+
+/* Sets the left child node.
+ * @param BSTNodeT* - a pointer to the left child node.
+ */
+template <typename T>
+void BSTNodeT<T>::SetLeft(BSTNodeT* left_child) {
+  left_child_ = left_child;
+}
+
+/* Sets the right child node.
+ * @param BSTNodeT* - a pointer to the right child node.
+ */
+template <typename T>
+void BSTNodeT<T>::SetRight(BSTNodeT* right_child) {
+  right_child_ = right_child;
+}
+
+/* Gets a reference to a pointer to the left child.
+ * @return BSTNodeT*& - a reference to the pointer to the left child.
+ */
+template <typename T>
+BSTNodeT<T>*& BSTNodeT<T>::GetLeft() {
+  return left_child_;
+}
+
+/* Gets a reference to a pointer to the right child.
+ * @return BSTNodeT*& - a reference to the pointer to the right child.
+ */
+template <typename T>
+BSTNodeT<T>*& BSTNodeT<T>::GetRight() {
+  return right_child_;
+}
+
+/* Increments count by 1.
+ */
+template <typename T>
+void BSTNodeT<T>::IncrementCount() {
+  count_++;
+}
+
+/* Decrements count by 1.
+ */
+template <typename T>
+void BSTNodeT<T>::DecrementCount() {
+  count_--;
+}
+
+/* Gets the value of count.
+ */
+template <typename T>
+int BSTNodeT<T>::GetCount() {
+  return count_;
+}
+
 #endif  // BSTNODET_LOPES
